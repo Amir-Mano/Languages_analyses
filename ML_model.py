cecd80af8ee0ab6180b88f3feb8ab141c47f605c
Amir-Mano <amirmano14@gmail.com>
2024-02-13 15:54:11 +0200

    13th commit

--- ML_model.py
+++ ML_model.py
@@ -9,27 +9,53 @@ from sklearn.linear_model import Lasso
 from sklearn.linear_model import LinearRegression
 
 # load MD data
-lan_5 = sio.loadmat('MD_vectors.mat')
-lan_5 = lan_5['MD_vectors_all'][:,:33]
+lan_5 = sio.loadmat('means.mat')
+lan_5 = lan_5['MEAN_nMD_pre']
 Y = np.ones((lan_5.shape[1],1))*5
 
-lan_4 = sio.loadmat('MD_vectors.mat')
-lan_4 = lan_4['MD_vectors_all'][:,34:]
+lan_4 = sio.loadmat('means.mat')
+lan_4 = lan_4['MEAN_nMD_post']
 Y = np.vstack((Y, np.ones((lan_4.shape[1],1))*4))
+
+# load MD data
+lan_2 = sio.loadmat('means.mat')
+lan_2 = lan_2['MEAN_nMD_pre']*-0.2
+Y = np.vstack((Y, np.ones((lan_2.shape[1],1))*2))
+
+lan_1 = sio.loadmat('means.mat')
+lan_1 = lan_1['MEAN_nMD_post']*0.2
+Y = np.vstack((Y, np.ones((lan_1.shape[1],1))*1))
 
 # Train/Test Split
 Y = Y.squeeze()
-X = np.hstack((lan_5[0], lan_4)).T
-X = pd.DataFrame(X)
+X = np.hstack((lan_5, lan_4, lan_2, lan_1)).T
+X = X[:,np.where(np.isnan(X).sum(axis=0) == 0)[0]]
 
 # Model Creation
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-model = LinearRegression()
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+model = Lasso(alpha=0.1)
 _ = model.fit(x_train, y_train)
 
 
 # Model Application¶
 y_pred = model.predict(x_test)
+
+# Model Evaluation
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import confusion_matrix
+mean_squared_error(y_test, y_pred)
+confusion_matrix(y_test, y_pred.round())
+
+# plot the results and confusion matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.heatmap(confusion_matrix(y_test, y_pred.round()), annot=True, cmap='Greens', cbar=False)
+plt.show()
+
+
+
+
+
 
 
 
